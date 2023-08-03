5252ec1d476e01b68088d682782ade704cef7ae1
dimasikpsk <139615397+dimasikpsk@users.noreply.github.com>
2023-08-03 13:16:06 +0300

    Update 2.js

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 const clockContainer = document.querySelector('.js-clock'),
       clockTitle = clockContainer.querySelector('h1');
-
+ 
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
