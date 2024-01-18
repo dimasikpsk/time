ba8f2f45a39217b114a2bd8a6b4dd607ed03ee84
dimasikpsk <139615397+dimasikpsk@users.noreply.github.com>
2024-01-18 09:36:38 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -38,7 +38,24 @@ init();
 
 
 
+function init() {
+    getTime();
+}
+svbfebngnb
+init();
 
+
+sdfdwdwasdcwwdscsa
+
+const clockContainer = document.querySelector('.js-clock'),
+      clockTitle = clockContainer.querySelector('h1');
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`;
 
 <?php
 // Создаём просто массив.
